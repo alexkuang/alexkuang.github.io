4e79c777f41bbd6e17c5017fd63e55c6294b49e0
alex <alex.kuang.13@gmail.com>
2023-01-30 07:48:16 -0500

    syntax highlighting basics

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,6 @@
-const { DateTime } = require("luxon")
+const { DateTime } = require("luxon");
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+
 module.exports = function(eleventyConfig) {
   // Output directory: _site
   eleventyConfig.addPassthroughCopy("assets");
@@ -6,7 +8,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addFilter("postDate", (d) => {
     return DateTime.fromJSDate(d).toUTC().toLocaleString(DateTime.DATE_MED);
   });
+  eleventyConfig.addPlugin(syntaxHighlight);
 
-  return {
-  }
+  return {};
 };
