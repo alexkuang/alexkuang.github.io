57272b9642545e24a9624be25cdbc475cf9527b3
alex <alex.kuang.13@gmail.com>
2025-12-09 17:53:46 -0500

    fiddle with domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://bikeshed.coffee',
+  site: 'https://makeshift.computer',
   vite: {
     plugins: [tailwindcss()]
   }
